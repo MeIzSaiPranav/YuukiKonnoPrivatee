f80958f5112b497bb53336fd6cc0ed584e1362ca
MeIzSaiPranav <86369504+MeIzSaiPranav@users.noreply.github.com>
2021-09-19 15:04:38 +0530

    Update logomaker.py

--- EmiliaAnimeBot/modules/logomaker.py
+++ EmiliaAnimeBot/modules/logomaker.py
@@ -203,50 +203,10 @@ async def lego(event):
    await event.reply(f'Error Report @PigasusSupport, {e}')
  
  
- 
- 
- @register(pattern="^/marshmallow_logo ?(.*)")
-async def lego(event):
- quew = event.pattern_match.group(1)
- if event.sender_id == OWNER_ID:
-     pass
- else:
-     
-    if not quew:
-       await event.reply('Provide Some Text To Draw!')
-       return
-    else:
-       pass
- await event.reply('Creating your logo...wait!')
- try:
-    text = event.pattern_match.group(1)
-    img = Image.open('./EmiliaAnimeBot/resources/marshmalloww.jpg')
-    draw = ImageDraw.Draw(img)
-    image_widthz, image_heightz = img.size
-    pointsize = 399
-    fillcolor = "gold"
-    shadowcolor = "blue"
-    font = ImageFont.truetype("./EmiliaAnimeBot/resources/TypoGraphica_demo.otf", 85)
-    w, h = draw.textsize(text, font=font)
-    h += int(h*0.21)
-    image_width, image_height = img.size
-    draw.text(((image_widthz-w)/1, (image_heightz-h)/2), text, font=font, fill=(327, 222, 222))
-    x = (image_widthz-w)/2
-    y= ((image_heightz-h)/2+6)
-    draw.text((x, y), text, font=font, fill="White", stroke_width=7, stroke_fill="Blue")
-    fname2 = "LogoByYone.png"
-    img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By @Me_iz_mad_boi")
-    if os.path.exists(fname2):
-            os.remove(fname2)
- except Exception as e:
-   await event.reply(f'Error Report @PigasusSupport, {e}')
-   
- 
- 
 file_help = os.path.basename(__file__)
 file_help = file_help.replace(".py", "")
 file_helpo = file_help.replace("_", " ")
+
 
 
 __help__ = """
