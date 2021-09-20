e1d5517d1ebf515859ec90ac635a9e45998f6dad
MeIzSaiPranav <86369504+MeIzSaiPranav@users.noreply.github.com>
2021-09-20 13:35:25 +0530

    Update logomaker.py

--- EmiliaAnimeBot/modules/logomaker.py
+++ EmiliaAnimeBot/modules/logomaker.py
@@ -245,45 +245,6 @@ async def lego(event):
  
  
  
- @register(pattern="^/hecker_logo ?(.*)")
-async def lego(event):
- quew = event.pattern_match.group(1)
- if event.sender_id == OWNER_ID:
-     pass
- else:
-     
-    if not quew:
-       await event.reply('Provide Some Text To Draw!')
-       return
-    else:
-       pass
- await event.reply('Creating your logo...wait!')
- try:
-    text = event.pattern_match.group(1)
-    img = Image.open('./EmiliaAnimeBot/resources/photo_2021-09-19_10-27-17 (3).jpg')
-    draw = ImageDraw.Draw(img)
-    image_widthz, image_heightz = img.size
-    pointsize = 399
-    fillcolor = "gold"
-    shadowcolor = "blue"
-    font = ImageFont.truetype("./EmiliaAnimeBot/resources/Deadly Advance.ttf", 85)
-    w, h = draw.textsize(text, font=font)
-    h += int(h*0.21)
-    image_width, image_height = img.size
-    draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(327, 222, 222))
-    x = (image_widthz-w)/2
-    y= ((image_heightz-h)/2+6)
-    draw.text((x, y), text, font=font, fill="White", stroke_width=7, stroke_fill="Blue")
-    fname2 = "LogoByYone.png"
-    img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By @Me_iz_mad_boi")
-    if os.path.exists(fname2):
-            os.remove(fname2)
- except Exception as e:
-   await event.reply(f'Error Report @PigasusSupport, {e}')
-   
-   
- 
 file_help = os.path.basename(__file__)
 file_help = file_help.replace(".py", "")
 file_helpo = file_help.replace("_", " ")
