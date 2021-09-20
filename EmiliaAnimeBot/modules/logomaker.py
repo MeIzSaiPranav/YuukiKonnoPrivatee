ac8cff41f03f9c571b53fc0154aa6f3a8685b36c
MeIzSaiPranav <86369504+MeIzSaiPranav@users.noreply.github.com>
2021-09-20 09:35:51 +0530

    Update logomaker.py

--- EmiliaAnimeBot/modules/logomaker.py
+++ EmiliaAnimeBot/modules/logomaker.py
@@ -230,8 +230,8 @@ async def lego(event):
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
-    draw.text(((image_widthz-w)/2, (image_heightz-h)/0), text, font=font, fill=(327, 222, 222))
-    x = (image_widthz-w)/2
+    draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(327, 222, 222))
+    x = (image_widthz-w)/3
     y= ((image_heightz-h)/2+6)
     draw.text((x, y), text, font=font, fill="White", stroke_width=7, stroke_fill="Blue")
     fname2 = "LogoByYone.png"
