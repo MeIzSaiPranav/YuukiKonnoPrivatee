d71ed94b7a488fcb93870fc29f0eeadd0a541daa
MeIzSaiPranav <86369504+MeIzSaiPranav@users.noreply.github.com>
2021-09-23 13:18:40 +0530

    Update anime.py

--- EmiliaAnimeBot/modules/anime.py
+++ EmiliaAnimeBot/modules/anime.py
@@ -557,70 +557,6 @@ def kayo(update: Update, context: CallbackContext):
     
     
 
- @pbot.on_message(filters.command("nhentai")
-async def nhentai(_, message):
-    if len(message.command) < 2:
-        await message.delete()
-        return
-    query = message.text.split(None, 1)[1]
-    title, tags, artist, total_pages, post_url, cover_image = nhentai_data(
-        query)
-    await message.reply_text(
-        f"<code>{title}</code>\n\n<b>Tags:</b>\n{tags}\n<b>Artists:</b>\n{artist}\n<b>Pages:</b>\n{total_pages}",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "Read Here",
-                        url=post_url
-                    )
-                ]
-            ]
-        )
-    )
-
-
-def nhentai_data(noombers):
-    url = f"https://nhentai.net/api/gallery/{noombers}"
-    res = requests.get(url).json()
-    pages = res["images"]["pages"]
-    info = res["tags"]
-    title = res["title"]["english"]
-    links = []
-    tags = ""
-    artist = ''
-    total_pages = res['num_pages']
-    extensions = {
-        'j': 'jpg',
-        'p': 'png',
-        'g': 'gif'
-    }
-    for i, x in enumerate(pages):
-        media_id = res["media_id"]
-        temp = x['t']
-        file = f"{i+1}.{extensions[temp]}"
-        link = f"https://i.nhentai.net/galleries/{media_id}/{file}"
-        links.append(link)
-
-    for i in info:
-        if i["type"] == "tag":
-            tag = i['name']
-            tag = tag.split(" ")
-            tag = "_".join(tag)
-            tags += f"#{tag} "
-        if i["type"] == "artist":
-            artist = f"{i['name']} "
-
-    post_content = "".join(f"<img src={link}><br>" for link in links)
-
-    post = telegraph.create_page(
-        f"{title}",
-        html_content=post_content,
-        author_name="@YuukiKonnoRobot",
-        author_url="https://t.me/YuukiKonnoRobot"
-    )
-    return title, tags, artist, total_pages, post['url'], links[0]
-
 
 __help__ = """
 Get information about anime, manga or characters from [AniList](anilist.co).
@@ -634,7 +570,6 @@ Get information about anime, manga or characters from [AniList](anilist.co).
  • `/upcoming`*:* returns a list of new anime in the upcoming seasons.
  • `/kaizoku <anime>`*:* search an anime on animekaizoku.com
  • `/kayo <anime>`*:* search an anime on animekayo.com
- • `/nhentai` <ID>: returns information from nhentai in telegraph page.
  • `/airing <anime>`*:* returns anime airing info.
 
  """
@@ -648,7 +583,6 @@ UPCOMING_HANDLER = DisableAbleCommandHandler("upcoming", upcoming)
 KAIZOKU_SEARCH_HANDLER = DisableAbleCommandHandler("kaizoku", kaizoku)
 KAYO_SEARCH_HANDLER = DisableAbleCommandHandler("kayo", kayo)
 BUTTON_HANDLER = CallbackQueryHandler(button, pattern='anime_.*')
-NHENTAI_HANDLER = DisableAbleCommandHandler("nhentai", nhentai)
 
 dispatcher.add_handler(BUTTON_HANDLER)
 dispatcher.add_handler(ANIME_HANDLER)
@@ -659,7 +593,6 @@ dispatcher.add_handler(USER_HANDLER)
 dispatcher.add_handler(KAIZOKU_SEARCH_HANDLER)
 dispatcher.add_handler(KAYO_SEARCH_HANDLER)
 dispatcher.add_handler(UPCOMING_HANDLER)
-dispatcher.add_handler(NHENTAI_HANDLER)
 
 __mod_name__ = "Anime"
 __command_list__ = [
@@ -669,5 +602,5 @@ __command_list__ = [
 __handlers__ = [
     ANIME_HANDLER, CHARACTER_HANDLER, MANGA_HANDLER, USER_HANDLER,
     UPCOMING_HANDLER, KAIZOKU_SEARCH_HANDLER, KAYO_SEARCH_HANDLER,
-    BUTTON_HANDLER, AIRING_HANDLER, NHENTAI_HANDLER
+    BUTTON_HANDLER, AIRING_HANDLER
 ]
