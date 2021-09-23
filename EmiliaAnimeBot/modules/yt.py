d08b65c5f14e2d332df8231631621087f67a4a1a
MeIzSaiPranav <86369504+MeIzSaiPranav@users.noreply.github.com>
2021-09-23 16:09:31 +0530

    Update yt.py

--- EmiliaAnimeBot/modules/yt.py
+++ EmiliaAnimeBot/modules/yt.py
@@ -17,7 +17,7 @@ from youtube_dl.utils import (
 from EmiliaAnimeBot.events import register as Yuuki
 
 
-@Mizuki(pattern="^/yt(audio|video) (.*)")
+@Yuuki(pattern="^/yt(audio|video) (.*)")
 async def download_video(v_url):
     """ For ytdl command, download media from YouTube and many other sites. """
     url = v_url.pattern_match.group(2)
