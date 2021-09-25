b8e62126493547dfa897acade6b41c42dfc48cf5
MeIzSaiPranav <86369504+MeIzSaiPranav@users.noreply.github.com>
2021-09-25 17:00:29 +0530

    Update Vcplayer.py

--- EmiliaAnimeBot/modules/Vcplayer.py
+++ EmiliaAnimeBot/modules/Vcplayer.py
@@ -6,30 +6,85 @@ I can now also play music in your group's voice chat
  
 Assistant username:- @YuukiXMusic
  
- Setting up 
-1) Make bot admin
+ Setting up
+1) Make bot admin (Group and in channel if use cplay)
 2) Start a voice chat
-3) Try `/play [song name]` for the first time by an admin
-) If userbot joined enjoy music, If not add @YuukiXMusic to your group manually and retry
- Commands:
- 
-• Song Playing 🎶
-  `/play` <songname>: Play song using youtube music
-  `/play` <yt url> : Play the given yt url
-  `/dplay`: Play song via deezer
-  `/splay`: Play song via jio saavn
-  
-• Playback ⏯
-  `/player`: Open Settings menu of player
-  `/skip`: Skips the current track
-  `/pause`: Pause track
-  `/resume`: Resumes the paused track
-  `/end`: Stops media playback
-  `/current`: Shows the current Playing track
-  `/playlist`: Shows playlist
-• More tools 📲
-  `/admincache`: Updates admin info of your group. Try if bot isn't recognize admin
-  `/userbotjoin`: Invite @GokuMusicHelper to your chat
-Player cmd and all other cmds except /play, /current and /playlist are only for admins with manage group
- PLEASE NOTE THIS SERVICE IS UNSTABLE AND CAN BE STOPPED ANYTIME 
+3) Try /play [song name] for the first time by an admin
+*) If userbot joined enjoy music, If not add @{ASSISTANT_NAME} to your group and retry
+For Channel Music Play
+1) Make me admin of your channel 
+2) Send /userbotjoinchannel in linked group
+3) Now send commands in linked group
+Join @PigasusUpdates
+""",
+f"""
+Commands
+=>> Song Playing 🎧
+- /play: Play the requestd song
+- /play [yt url] : Play the given yt url
+- /play [reply yo audio]: Play replied audio
+- /splay: Play song via jio saavn
+- /ytplay: Directly play song via Youtube Music
+=>> Playback ⏯
+- /player: Open Settings menu of player
+- /skip: Skips the current track
+- /pause: Pause track
+- /resume: Resumes the paused track
+- /end: Stops media playback
+- /current: Shows the current Playing track
+- /playlist: Shows playlist
+*Player cmd and all other cmds except /play, /current  and /playlist  are only for admins of the group.
+Join @PigasusUpdates
+""",
+
+f"""
+=>> Channel Music Play 🛠
+⚪️ For linked group admins only:
+- /cplay [song name] - play song you requested
+- /csplay [song name] - play song you requested via jio saavn
+- /cplaylist - Show now playing list
+- /cccurrent - Show now playing
+- /cplayer - open music player settings panel
+- /cpause - pause song play
+- /cresume - resume song play
+- /cskip - play next song
+- /cend - stop music play
+- /userbotjoinchannel - invite assistant to your chat
+channel is also can be used instead of c ( /cplay = /channelplay )
+⚪️ If you donlt like to play in linked group:
+1) Get your channel ID.
+2) Create a group with tittle: Channel Music: your_channel_id
+3) Add bot as Channel admin with full perms
+4) Add @{ASSISTANT_NAME} to the channel as an admin.
+5) Simply send commands in your group. (remember to use /ytplay instead /play)
+Join @PigasusUpdates
+""",
+
+f"""
+=>> More tools 🧑‍🔧
+- /musicplayer [on/off]: Enable/Disable Music player
+- /admincache: Updates admin info of your group. Try if bot isn't recognize admin
+- /userbotjoin: Invite @YuukiXMusic Userbot to your chat
+Join @PigasusUpdates
+""",
+f"""
+=>> Song Download 🎸
+- /video [song mame]: Download video song from youtube
+- /song [song name]: Download audio song from youtube
+- /saavn [song name]: Download song from saavn
+- /deezer [song name]: Download song from deezer
+=>> Search Tools 📄
+- /search [song name]: Search youtube for songs
+- /lyrics [song name]: Get song lyrics
+Join @PigasusUpdates
+""",
+
+f"""
+=>> Commands for Sudo Users ⚔️
+ - /userbotleaveall - remove assistant from all chats
+ - /broadcast <reply to message> - globally brodcast replied message to all chats
+ - /pmpermit [on/off] - enable/disable pmpermit message
+*Sudo Users can execute any command in any groups
+Join @PigasusUpdates
 """
+      ]
