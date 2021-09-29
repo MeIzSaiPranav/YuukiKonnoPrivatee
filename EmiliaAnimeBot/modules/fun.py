7c30b57a6381f7806ba3922cd35f4d1edbf404ae
MeIzSaiPranav <86369504+MeIzSaiPranav@users.noreply.github.com>
2021-09-29 15:38:13 +0530

    Update fun.py

--- EmiliaAnimeBot/modules/fun.py
+++ EmiliaAnimeBot/modules/fun.py
@@ -2,18 +2,18 @@ import html
 import random
 import time
 
-import Sophia.modules.fun_strings as fun_strings
-from Sophia import dispatcher
-from Sophia.modules.disable import DisableAbleCommandHandler, DisableAbleMessageHandler
-from Sophia.modules.helper_funcs.chat_status import is_user_admin
-from Sophia.modules.helper_funcs.alternate import typing_action
-from Sophia.modules.helper_funcs.filters import CustomFilters
-from Sophia.modules.helper_funcs.extraction import extract_user
+import EmiliaAnimeBot.modules.fun_strings as fun_strings
+from EmiliaAnimeBot import dispatcher
+from EmiliaAnimeBot.modules.disable import DisableAbleCommandHandler, DisableAbleMessageHandler
+from EmiliaAnimeBot.modules.helper_funcs.chat_status import is_user_admin
+from EmiliaAnimeBot.modules.helper_funcs.alternate import typing_action
+from EmiliaAnimeBot.modules.helper_funcs.filters import CustomFilters
+from EmiliaAnimeBot.modules.helper_funcs.extraction import extract_user
 from telegram import ChatPermissions, ParseMode, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, run_async, CommandHandler, Filters
 
-import Sophia.modules.helper_funcs.string_store as fun
+import EmiliaAnimeBot.modules.helper_funcs.string_store as fun
 
 GIF_ID = "CgACAgQAAx0CSVUvGgAC7KpfWxMrgGyQs-GUUJgt-TSO8cOIDgACaAgAAlZD0VHT3Zynpr5nGxsE"
 
