c9594de9c841c578c12f8751f28744a7992a3581
MeIzSaiPranav <86369504+MeIzSaiPranav@users.noreply.github.com>
2021-10-01 13:21:40 +0530

    Update covid.py

--- EmiliaAnimeBot/modules/covid.py
+++ EmiliaAnimeBot/modules/covid.py
@@ -12,11 +12,11 @@ def covid(update: Update, context: CallbackContext):
     text = message.text.split(" ", 1)
     if len(text) == 1:
         r = requests.get("https://corona.lmao.ninja/v2/all").json()
-        reply_text = f"**Global Totals** 🦠\nCases: {r['cases']:,}\nCases Today: {r['todayCases']:,}\nDeaths: {r['deaths']:,}\nDeaths Today: {r['todayDeaths']:,}\nRecovered: {r['recovered']:,}\nActive: {r['active']:,}\nCritical: {r['critical']:,}\nCases/Mil: {r['casesPerOneMillion']}\nDeaths/Mil: {r['deathsPerOneMillion']}"
+        reply_text = f"**Global Totals** 🦠\nCases: {r['🥺cases']:,}\n😟Cases Today: {r['todayCases']:,}\n⚰️Deaths: {r['deaths']:,}\n⚰️Deaths Today: {r['todayDeaths']:,}\n😊Recovered: {r['recovered']:,}\n😔Active: {r['active']:,}\n⚠️Critical: {r['critical']:,}\nCases/Mil: {r['casesPerOneMillion']}\nDeaths/Mil: {r['deathsPerOneMillion']}"
     else:
         variabla = text[1]
         r = requests.get(f"https://corona.lmao.ninja/v2/countries/{variabla}").json()
-        reply_text = f"**Cases for {r['country']} 🦠**\nCases: {r['cases']:,}\nCases Today: {r['todayCases']:,}\nDeaths: {r['deaths']:,}\nDeaths Today: {r['todayDeaths']:,}\nRecovered: {r['recovered']:,}\nActive: {r['active']:,}\nCritical: {r['critical']:,}\nCases/Mil: {r['casesPerOneMillion']}\nDeaths/Mil: {r['deathsPerOneMillion']}"
+        reply_text = f"**Cases for {r['country']} 🦠**\nCases: {r['cases']:,}\n😟Cases Today: {r['todayCases']:,}\n⚰️Deaths: {r['deaths']:,}\n⚰️Deaths Today: {r['todayDeaths']:,}\n😊Recovered: {r['recovered']:,}\n😔Active: {r['active']:,}\n⚠️Critical: {r['critical']:,}\nCases/Mil: {r['casesPerOneMillion']}\nDeaths/Mil: {r['deathsPerOneMillion']}"
     message.reply_text(reply_text, parse_mode=ParseMode.MARKDOWN)
 
 
