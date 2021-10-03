8cfbc0cf7937411e1c4731dcee18460fc88df512
MeIzSaiPranav <86369504+MeIzSaiPranav@users.noreply.github.com>
2021-10-03 13:48:04 +0530

    Update core.py

--- EmiliaAnimeBot/modules/core.py
+++ EmiliaAnimeBot/modules/core.py
@@ -1,16 +1,16 @@
 from Sophia import telethn as tbot
-from Sophia.events import register
+from EmiliaAnimeBot.events import register
 import os
 import asyncio
 import os
 import time
 
 from datetime import datetime
-from Sophia import OWNER_ID, DEV_USERS
-from Sophia import TEMP_DOWNLOAD_DIRECTORY as path
-from Sophia import TEMP_DOWNLOAD_DIRECTORY
+from EmiliaAnimeBot import OWNER_ID, DEV_USERS
+from EmiliaAnimeBot import TEMP_DOWNLOAD_DIRECTORY as path
+from EmiliaAnimeBot import TEMP_DOWNLOAD_DIRECTORY
 from datetime import datetime
-water = './Sophia/resources/serena.jpg'
+water = './EmiliaAnimeBot/resources/photo_2021-10-03_13-46-33.jpg'
 client = tbot
 
 @register(pattern=r"^/send ?(.*)")
@@ -22,7 +22,7 @@ async def Prof(event):
     thumb = water
     message_id = event.message.id
     input_str = event.pattern_match.group(1)
-    the_plugin_file = "./Sophia/modules/{}.py".format(input_str)
+    the_plugin_file = "./EmiliaAnimeBot/modules/{}.py".format(input_str)
     if os.path.exists(the_plugin_file):
      message_id = event.message.id
      await event.client.send_file(
