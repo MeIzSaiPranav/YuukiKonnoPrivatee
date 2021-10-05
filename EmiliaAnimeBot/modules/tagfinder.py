c655cd86468230941b955a06d72b5fbea2b7dca5
MeIzSaiPranav <86369504+MeIzSaiPranav@users.noreply.github.com>
2021-10-05 11:04:58 +0530

    Create tagfinder.py

---
+++ EmiliaAnimeBot/modules/tagfinder.py
@@ -0,0 +1,12 @@
+import os
+import pyrogram
+from pyrogram import Client, filters
+import YoutubeTags 
+from YoutubeTags import videotags
+from EmiliaAnimeBot import pbot 
+from EmiliaAnimeBot.events import register
+
+@register(pattern="^/tags$")
+async def virusscan(event):
+    tags = videotags(link) 
+    await message.reply_text("**These are the Tags that I Found** \n\n ` {tags} `"
