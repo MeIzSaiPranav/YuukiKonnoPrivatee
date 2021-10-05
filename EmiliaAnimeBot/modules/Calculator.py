314fe9c12820a3079f8051a066ffff6ccf19f13b
MeIzSaiPranav <86369504+MeIzSaiPranav@users.noreply.github.com>
2021-10-05 10:53:12 +0530

    Create Calculator.py

---
+++ EmiliaAnimeBot/modules/Calculator.py
@@ -0,0 +1,46 @@
+import os
+from pyrogram import Client, filters
+from pyrogram.types import *
+
+from EmiliaAnimeBot.conf import get_str_key
+from EmiliaAnimeBot import pbot
+
+CALCULATE_TEXT = "simple calculator\n\n@YuukiKonnoRobot"
+
+CALCULATE_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton("DEL", callback_data="DEL"),
+        InlineKeyboardButton("AC", callback_data="AC"),
+        InlineKeyboardButton("(", callback_data="("),
+        InlineKeyboardButton(")", callback_data=")")
+        ],[
+        InlineKeyboardButton("7", callback_data="7"),
+        InlineKeyboardButton("8", callback_data="8"),
+        InlineKeyboardButton("9", callback_data="9"),
+        InlineKeyboardButton("÷", callback_data="/")
+        ],[
+        InlineKeyboardButton("4", callback_data="4"),
+        InlineKeyboardButton("5", callback_data="5"),
+        InlineKeyboardButton("6", callback_data="6"),
+        InlineKeyboardButton("×", callback_data="*")
+        ],[
+        InlineKeyboardButton("1", callback_data="1"),
+        InlineKeyboardButton("2", callback_data="2"),
+        InlineKeyboardButton("3", callback_data="3"),
+        InlineKeyboardButton("-", callback_data="-"),
+        ],[
+        InlineKeyboardButton(".", callback_data="."),
+        InlineKeyboardButton("0", callback_data="0"),
+        InlineKeyboardButton("=", callback_data="="),
+        InlineKeyboardButton("+", callback_data="+"),
+        ]]
+    )
+
+@pbot.on_message(filters.command(["calculator"]))
+async def calculate(pbot, update):
+    await update.reply_text(
+        text=CALCULATE_TEXT,
+        reply_markup=CALCULATE_BUTTONS,
+        disable_web_page_preview=True,
+        quote=True
+    )
