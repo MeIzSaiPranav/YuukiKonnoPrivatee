bb0f154b4b00148e7a86c2053ccf31a19419e31c
MeIzSaiPranav <86369504+MeIzSaiPranav@users.noreply.github.com>
2021-10-06 14:11:48 +0530

    Create prankpromote.py

---
+++ EmiliaAnimeBot/modules/prankpromote.py
@@ -0,0 +1,16 @@
+import asyncio
+
+from . import *
+
+from EmiliaAnimeBot.events import register
+
+
+@register(pattern="ppromote")
+async def pprank(ult):
+    msg = await eor(ult, "**PROMOTING USER..**")
+    await asyncio.sleep(1)
+    await msg.edit("**PROMOTING USER...**")
+    await asyncio.sleep(1)
+    await msg.edit("**GIVING RIGHTS**")
+    await asyncio.sleep(1)
+    await msg.edit("**PROMOTED USER SUCCESSFULLY**")
