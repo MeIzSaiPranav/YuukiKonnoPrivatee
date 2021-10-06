fc91ce323f0f303a0ec2f7bb60eda9f04ad8b8ba
MeIzSaiPranav <86369504+MeIzSaiPranav@users.noreply.github.com>
2021-10-06 13:42:28 +0530

    Create aniquote.py

---
+++ EmiliaAnimeBot/modules/aniquote.py
@@ -0,0 +1,19 @@
+import requests
+from telegram import ParseMode, Update
+from telegram.ext import CallbackContext, run_async
+
+from EmiliaAnimeBot import dispatcher
+from EmiliaAnimeBot.modules.disable import DisableAbleCommandHandler
+
+
+@run_async
+def aniquote(update: Update, context: CallbackContext):
+    message = update.effective_message
+    text = message.text.split(" ", 1)
+    if len(text) == 1:
+        r = requests.get("https://animechan.vercel.app/api/random").json()
+        reply_text = f"**{resp['quote']}**\n"
+        results += f" — __{resp['character']} ({resp['anime']})__"
+        return await u.edit(results)
+    except Exception:
+        await u.edit("`Something went wrong LOL ...`")
