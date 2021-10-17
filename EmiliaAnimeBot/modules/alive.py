a261fd37adc40501e8c3faac1aff5ef42d9848bc
MeIzSaiPranav <86369504+MeIzSaiPranav@users.noreply.github.com>
2021-10-17 21:59:24 +0530

    Update alive.py

--- EmiliaAnimeBot/modules/alive.py
+++ EmiliaAnimeBot/modules/alive.py
@@ -5,6 +5,7 @@ from EmiliaAnimeBot.events import register
 from EmiliaAnimeBot import telethn as tbot
 from EmiliaAnimeBot import telethn as tgbot
 PHOTO = random.choice(["https://telegra.ph//file/7f3ae4b9105c001e5ac59.jpg","https://telegra.ph/file/9ce5e51b9f4a531ea830b.jpg","https://telegra.ph//file/24df0a9ca50f11f8a3bc2.jpg","https://telegra.ph//file/a3545f201acb627a02bed.jpg","https://telegra.ph//file/dce0a6f265ab823053059.jpg"])
+
 @register(pattern=("/alive"))
 async def awake(event):
   PIKACHU = event.sender.first_name
