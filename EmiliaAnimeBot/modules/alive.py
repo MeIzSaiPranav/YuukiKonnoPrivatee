4c10cfeeb9ce39406d4fa1a8d6b356de7fbe48e2
TAMILVIP007 <79161058+TAMILVIP007@users.noreply.github.com>
2021-10-18 12:11:31 +0530

    update alive.py

--- EmiliaAnimeBot/modules/alive.py
+++ EmiliaAnimeBot/modules/alive.py
@@ -1,25 +1,42 @@
 from telethon import events, Button, custom
 import re, os
 import random
+import asyncio
 from EmiliaAnimeBot.events import register
 from EmiliaAnimeBot import telethn as tbot
+==========================================
 PHOTO = "https://telegra.ph/file/ae43f3254f76d0f7f9cf7.jpg"
+fuck = "https://telegra.ph//file/7f3ae4b9105c001e5ac59.jpg"
+sex = "https://telegra.ph//file/24df0a9ca50f11f8a3bc2.jpg"
+lmao = "https://telegra.ph//file/a3545f201acb627a02bed.jpg"
+lmao2 = "https://telegra.ph//file/dce0a6f265ab823053059.jpg"
 
+==========================================
 @register(pattern=("/alive"))
 async def awake(event):
   PIKACHU = event.sender.first_name
-  PIKACHU = "♡ I,m Yuuki💖 \n\n"
-  PIKACHU += "♡ I'm Working Properly**\n\n"
+  PIKACHU = "**♡ I,m Yuuki💖** \n\n"
+  PIKACHU += "**♡ I'm Working Properly**\n\n"
   PIKACHU += "**♡ Yuuki : 2.0 LATEST**\n\n"
-  PIKACHU += "**♡ My Master : [Madboi](t.me/Me_Iz_mad_boi)\n\n"
-  PIKACHU += "♡ Telethon Version : 1.23.0**\n\n"
+  PIKACHU += "**♡ My Master :** [Madboi](t.me/Me_Iz_mad_boi)\n\n"
+  PIKACHU += "**♡ Telethon Version : 1.23.0**\n\n"
   BUTTON = [[Button.url("𝙎𝙐𝙋𝙋𝙊𝙍𝙏", "https://t.me/PigasusSupport"), Button.url("𝙐𝙋𝘿𝘼𝙏𝙀", "https://t.me/PigasusUpdates")]]
-  await tbot.send_file(event.chat_id, PHOTO, caption=PIKACHU,  buttons=BUTTON)
+  on = await tbot.send_file(event.chat_id, PHOTO, caption=PIKACHU,  buttons=BUTTON)
+  await asyncio.sleep(5)
+   ok = await event.edit_message(yes.chat_id, on, file=fuck, buttons=BUTTON) 
 
+    await asyncio.sleep(5)
+    ok2 = await event.edit_message(yes.chat_id, ok, file=sex, buttons=BUTTON)
+
+    await asyncio.sleep(5)
+    ok3 = await event.edit_message(yes.chat_id, ok2, file=lmao, buttons=BUTTON)
+    
+    await asyncio.sleep(5)
+    ok4 = await event.edit_message(yes.chat_id, ok3, file=lmao2, buttons=BUTTON)
  
 @register(pattern=("/myinfo"))
 async def semx(event):
-  await tbot.send_message(event.chat_id, f'**➢ Hᴇʏ {(event.sender.first_name)}**\n\n**➢ I Aᴍ [Yᴜᴜᴋɪ](t.me/YuukiKonnoRobot)**\n**➢ I Wᴀs Cʀᴇᴀᴛᴇᴅ Bʏ [Sᴀɪ](t.me/Me_Iz_Mad_Boi)', file='cute.jpg', buttons=[Button.inline('Iɴғᴏ', data="sex"), Button.url('Sᴜᴘᴘᴏʀᴛ', 'https://t.me/YuukiSupportChat')], reply_to=event)
+  await tbot.send_message(event.chat_id, f'**➢ Hᴇʏ {(event.sender.first_name)}**\n\n**➢ I Aᴍ [Yᴜᴜᴋɪ](t.me/YuukiKonnoRobot)**\n**➢ I Wᴀs Cʀᴇᴀᴛᴇᴅ Bʏ [Sᴀɪ](t.me/Me_Iz_Mad_Boi)**', file='cute.jpg', buttons=[Button.inline('**Iɴғᴏ**', data="sex"), Button.url('**Sᴜᴘᴘᴏʀᴛ**', 'https://t.me/YuukiSupportChat')], reply_to=event)
   
 @tbot.on(events.CallbackQuery(pattern=r"sex"))
 async def ok(event):
