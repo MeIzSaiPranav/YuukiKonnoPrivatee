112ea8853246e9dba156605de539bec25fc2a922
MeIzSaiPranav <86369504+MeIzSaiPranav@users.noreply.github.com>
2021-10-18 14:52:40 +0530

    Update __main__.py

--- EmiliaAnimeBot/__main__.py
+++ EmiliaAnimeBot/__main__.py
@@ -697,11 +697,10 @@ def main():
 
     if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
         try:
-            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "💖 I'm Alive Now 💖")(https://telegra.ph/file/e74bb8ebb7025de7857bf.jpg)", parse_mode=ParseMode.MARKDOWN) 
+            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "[💖 I'm Alive Now 💖](https://telegra.ph/file/e74bb8ebb7025de7857bf.jpg)", parse_mode=ParseMode.MARKDOWN) 
         except Unauthorized:
             LOGGER.warning(
-                "Bot isnt able to send message to support_chat, go and check!"
-            )
+                "Bot isnt able to send message to support_chat, go and check!")
         except BadRequest as e:
             LOGGER.warning(e.message)
 
