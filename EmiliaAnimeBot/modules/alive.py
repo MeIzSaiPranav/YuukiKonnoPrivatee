31e76b7e915f795c30f4a37a2578fb71f08b8f68
MeIzSaiPranav <86369504+MeIzSaiPranav@users.noreply.github.com>
2021-10-20 17:34:44 +0530

    Update alive.py

--- EmiliaAnimeBot/modules/alive.py
+++ EmiliaAnimeBot/modules/alive.py
@@ -1,38 +1,83 @@
 from telethon import events, Button, custom
 import re, os
 import random
+import datetime
+import time
+import requests
+from PIL import Image
+from io import BytesIO
+from datetime import datetime
+from EmiliaAnimeBot import telethn as yukki
+from telethon.tl.types import ChannelParticipantsAdmins
+from EmilaAnimeBot import StartTime, dispatcher
 import asyncio
 from EmiliaAnimeBot.events import register
 from EmiliaAnimeBot import telethn as tbot
-"""=====================LOL====================="""
-PHOTO = "https://telegra.ph/file/ae43f3254f76d0f7f9cf7.jpg"
-fuck = "https://telegra.ph//file/7f3ae4b9105c001e5ac59.jpg"
-sex = "https://telegra.ph/file/408ab930c9dec34367590.jpg"
-lmao = "https://telegra.ph//file/a3545f201acb627a02bed.jpg"
-lmao2 = "https://telegra.ph/file/53793cb19c971ee6868a5.jpg"
+edit_time = 10
+"""=====================YUKKI====================="""
+yukki1 = "https://telegra.ph/file/ae43f3254f76d0f7f9cf7.jpg"
+yukki2 = "https://telegra.ph//file/7f3ae4b9105c001e5ac59.jpg"
+yukki3 = "https://telegra.ph/file/408ab930c9dec34367590.jpg"
+yukki4 = "https://telegra.ph//file/a3545f201acb627a02bed.jpg"
+"""=====================YUKKI====================="""
 
-"""================LOL=========================="""
+START_TIME = datetime.utcnow()
+START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
+TIME_DURATION_UNITS = (
+    ('week', 60 * 60 * 24 * 7),
+    ('day', 60 * 60 * 24),
+    ('hour', 60 * 60),
+    ('min', 60),
+    ('sec', 1)
+)
+
+async def _human_time_duration(seconds):
+    if seconds == 0:
+        return 'inf'
+    parts = []
+    for unit, div in TIME_DURATION_UNITS:
+        amount, seconds = divmod(int(seconds), div)
+        if amount > 0:
+            parts.append('{} {}{}'
+                         .format(amount, unit, "" if amount == 1 else "s"))
+    return ', '.join(parts)
+
 @register(pattern=("/alive"))
-async def awake(event):
-  PIKACHU = event.sender.first_name
-  PIKACHU = "**♡ I,m Yuuki💖** \n\n"
-  PIKACHU += "**♡ I'm Working Properly**\n\n"
-  PIKACHU += "**♡ Yuuki : 2.0 LATEST**\n\n"
-  PIKACHU += "**♡ My Master :** [Madboi](t.me/Me_Iz_mad_boi)\n\n"
-  PIKACHU += "**♡ Telethon Version : 1.23.0**\n\n"
-  BUTTON = [[Button.url("𝙎𝙐𝙋𝙋𝙊𝙍𝙏", "https://t.me/PigasusSupport"), Button.url("𝙐𝙋𝘿𝘼𝙏𝙀", "https://t.me/PigasusUpdates")]]
-  on = await tbot.send_file(event.chat_id, PHOTO, caption=PIKACHU,  buttons=BUTTON)
-  await asyncio.sleep(5)
-  ok = await event.edit_message(yes.chat_id, on, file=fuck, buttons=BUTTON) 
+async def hmm(yes):
+    chat = await yes.get_chat()
+    await yes.delete()
+    current_time = datetime.utcnow()
+    uptime_sec = (current_time - START_TIME).total_seconds()
+    uptime = await _human_time_duration(int(uptime_sec))
+    yukki = "**♡ I,m Yuuki💖**\n\n"
+    yukki += "**♡ I'm Working Properly**\n\n"
+    yukki += f"**♡ Yukki :** `{uptime}`\n\n"
+    yukki += "**♡ My Master :** [Madboi](t.me/Me_Iz_mad_boi)`\n"
+    yukki += "**♡ Telethon Version : 1.23.0**\n"
+    BUTTON = [[Button.url("𝙎𝙐𝙋𝙋𝙊𝙍𝙏", "https://t.me/PigasusSupport"), Button.url("𝙐𝙋𝘿𝘼𝙏𝙀", "https://t.me/PigasusUpdates")]]
+    on = await borg.send_file(yes.chat_id, file=yukki1,caption=yukki, buttons=BUTTON)
 
-  await asyncio.sleep(5)
-  ok2 = await event.edit_message(yes.chat_id, ok, file=sex, buttons=BUTTON)
+    await asyncio.sleep(edit_time)
+    ok = await borg.edit_message(yes.chat_id, on, file=yukki2) 
 
-  await asyncio.sleep(5)
-  ok3 = await event.edit_message(yes.chat_id, ok2, file=lmao, buttons=BUTTON)
+    await asyncio.sleep(edit_time)
+    ok2 = await borg.edit_message(yes.chat_id, ok, file=yukki3)
 
-  await asyncio.sleep(5)
-  ok4 = await event.edit_message(yes.chat_id, ok3, file=lmao2, buttons=BUTTON)
+    await asyncio.sleep(edit_time)
+    ok3 = await borg.edit_message(yes.chat_id, ok2, file=yukki1)
+    
+    await asyncio.sleep(edit_time)
+    ok4 = await borg.edit_message(yes.chat_id, ok3, file=yukki3)
+    
+    await asyncio.sleep(edit_time)
+    ok5 = await borg.edit_message(yes.chat_id, ok4, file=yukki2)
+    
+    await asyncio.sleep(edit_time)
+    ok6 = await borg.edit_message(yes.chat_id, ok5, file=yukki1)
+    
+    await asyncio.sleep(edit_time)
+    ok7 = await borg.edit_message(yes.chat_id, ok6, file=yukki4)
+
 @register(pattern=("/myinfo"))
 async def semx(event):
   await tbot.send_message(event.chat_id, f'**➢ Hᴇʏ {(event.sender.first_name)}**\n\n**➢ I Aᴍ [Yᴜᴜᴋɪ](t.me/YuukiKonnoRobot)**\n**➢ I Wᴀs Cʀᴇᴀᴛᴇᴅ Bʏ [Sᴀɪ](t.me/Me_Iz_Mad_Boi)**', file='cute.jpg', buttons=[Button.inline('**Iɴғᴏ**', data="sex"), Button.url('**Sᴜᴘᴘᴏʀᴛ**', 'https://t.me/YuukiSupportChat')], reply_to=event)
